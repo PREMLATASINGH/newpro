b34695815aa970fc68abd60b1e0f2cd5b35b9e7e
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-07-07 02:06:42 -0400

    first commit

---
+++ newpro.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+df=pd.read_csv('data4.csv')
+print(df.head())
+print(df.isnull().sum())
+df['Sales'].fillna(df['Sales'].mean(), inplace=True)  
+print(df.isnull().sum())
+df['Value'].fillna(df['Value'].mean(), inplace=True)    
+print(df.isnull().sum())
